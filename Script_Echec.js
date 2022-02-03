767b6124e1ef5f9dd0eca82b0a6d2b621877b9ba
raphoRaph <98906836+raphoRaph@users.noreply.github.com>
2022-02-03 20:11:17 +0100

    1.2
    Ecriture de la Grille (avec couleur)

--- Script_Echec.js
+++ Script_Echec.js
@@ -1,7 +1,5 @@
 
 /* Boutton Jouer pour lancer la Grille */
-var button_play;
-
 function Start(){ // Boutton pour afficher la grille d'échec
     let btn = document.createElement("button");
     btn.id = String("Joue");
@@ -12,18 +10,49 @@ function Start(){ // Boutton pour afficher la grille d'échec
     btn.style.height = "200px";
     btn.style.border = "1px solid #202020";
     document.body.appendChild(btn);
-    document.getElementById("Jeu");
+    document.getElementById("Jeu").appendChild(btn);
 }
 /* */
-class Grille{
-    constructor(i,j) {
-        this.position = (i,j)
-
+var GrilleFond = []
+for (let i = 0; i < 10; i ++){
+    GrilleFond[i] = new Array(21)
+    for (let j = 0; j < 10; j++){
+        GrilleFond[i][j] = String(i) + String(j);
     }
 }
+/* Ecriture de la Grille d'Echec */
 function Tampon(){
-    console.log("ah")
+    Remove_Start()
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            let btn = document.createElement("button");
+            btn.id = String(i) + String(j);
+            let boutonCliqué = String(btn.id)
+            btn.onclick = function(){Clic(boutonCliqué)}
+            btn.innerHTML = GrilleFond[i][j];
+            btn.style.width = "150px";
+            btn.style.height = "150px";
+            btn.style.border = "0px";
+            
+            document.body.appendChild(btn);
+            document.getElementById("Jeu").appendChild(btn);
+            if ((i - j)%2 != 0){ /* Alternance des couleurs */
+                document.getElementById(String(i) + String(j)).style.backgroundColor = 'black';
+                btn.style.color = "black";
+            }
+            else{
+                document.getElementById(String(i) + String(j)).style.backgroundColor = 'white';
+                btn.style.color = "white";
+            }
+        }
+    }
 }
-
+/* */
+/* Suppression du Bouton Jouer */
+function Remove_Start(){
+        var elem = document.getElementById("Joue")
+        elem.parentNode.removeChild(elem);
+}
+/* */
 
 Start()
